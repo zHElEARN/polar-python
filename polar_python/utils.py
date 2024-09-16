b91c55d2a6b7afcc55400904992c8a2b83095890
zHElEARN <personal@zhelearn.com>
2024-09-16 22:07:02 +0800

    fix: correct RR interval calculation error

--- polar_python/utils.py
+++ polar_python/utils.py
@@ -155,7 +155,7 @@ def parse_heartrate_data(data: bytearray) -> constants.HRData:
         rr_intervals = [
             int.from_bytes(data[i : i + 2], byteorder="little", signed=False)
             / 1024.0
-            * 1024.0
+            * 1000.0
             for i in range(2, len(data), 2)
         ]
         return constants.HRData(heartrate, rr_intervals)
