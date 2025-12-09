cc9c199a4b63efea113dd5805236cf65116a37bb
zHElEARN <personal@zhelearn.com>
2025-12-09 20:36:15 +0800

    chore(release): bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="polar-python",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     install_requires=["bleak"],
     author="Zhe_Learn",
