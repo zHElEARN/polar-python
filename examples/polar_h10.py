e1256397f45606f6a11b94b0fff3950436d7559b
zHElEARN <personal@zhelearn.com>
2026-03-21 10:23:14 +0800

    refactor(examples): simplify exit handling and improve main loop in example scripts

--- examples/polar_h10.py
+++ examples/polar_h10.py
@@ -1,6 +1,4 @@
 import asyncio
-import signal
-import threading
 
 from bleak import BleakScanner
 from rich import inspect
@@ -10,12 +8,6 @@ from polar_python import PolarDevice
 from polar_python.models import ACCData, ECGData, HRData
 
 console = Console()
-exit_event = threading.Event()
-
-
-def handle_exit(*_):
-    console.print("[bold red]Received exit signal[/bold red]")
-    exit_event.set()
 
 
 async def main():
@@ -47,17 +39,14 @@ async def main():
         await polar_device.start_acc_stream(acc_callback=acc_callback, sample_rate=25, resolution=16, range=2)
         await polar_device.start_hr_stream(hr_callback=hr_callback)
 
-        while not exit_event.is_set():
+        while True:
             await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, handle_exit)
-    signal.signal(signal.SIGTERM, handle_exit)
-
-    loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(main())
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        console.print("[bold red]Received Ctrl+C[/bold red]")
     finally:
-        loop.close()
         console.print("[bold red]Program exited gracefully[/bold red]")
